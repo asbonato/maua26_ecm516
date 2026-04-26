67440676d7e92de5c45605364608134357ab2456
asbonato <asbonato@gmail.com>
2026-04-26 19:28:49 -0300

    feature(app_lembretes): adicionados mss lembretes e observacoes feito até seção 4.3.23 da apostila - retomar em busca da coleção de lembretes, pag. 31

---
+++ app_lembretes/lembretes/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const lembretes = {}
+let contador = 0
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes)
+})
+
+app.post('/lembretes', (req, res) => {
+    contador++
+    const { texto } = req.body
+    lembretes[contador] = {contador, texto}
+    res.status(200).send(lembretes[contador])
+})
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000.')
+})
