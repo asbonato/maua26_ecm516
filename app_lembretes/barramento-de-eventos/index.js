aa6d724348e0a9bb2fa09844b7d92e9486863941
asbonato <asbonato@gmail.com>
2026-05-09 10:19:03 -0300

    feature(app_lembretes): adicionados mss consulta e     barramento de eventos     feito até seção 4.3.39 da apostila -     retomar classificação de observações, pag. 48

---
+++ app_lembretes/barramento-de-eventos/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+app.use(express.json())
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body
+    //envia o evento para o microsserviço de lembretes
+    axios.post('http://localhost:4000/eventos', evento)
+    .catch((err) => {
+        console.log('Microsserviço de lembretes fora do ar.')
+    })
+    //envia o evento para o microsserviço de observações
+    axios.post('http://localhost:5001/eventos', evento)
+    .catch((err) => {
+        console.log('Microsserviço de observações fora do ar.')
+    })
+    //envia o evento para o microsserviço de consulta
+    axios.post('http://localhost:6000/eventos', evento)
+    .catch((err) => {
+        console.log('Microsserviço de consulta fora do ar.')
+    })
+    res.status(200).send({msg: 'ok'})
+})
+
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000.')
+})
--- app_lembretes/lembretes/index.js
+++ app_lembretes/lembretes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 app.use(express.json())
 
@@ -9,11 +10,23 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes)
 })
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     contador++
     const { texto } = req.body
     lembretes[contador] = {contador, texto}
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: 'LembreteCriado',
+        dados: {
+            contador,
+            texto
+        }
+    })
     res.status(200).send(lembretes[contador])
+})
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({msg: 'ok'})
 })
 
 app.listen(4000, () => {
