e620d2565c18c00edd486838e788a24de42ec5c5
asbonato <asbonato@gmail.com>
2026-09-05 20:01:11 -0300

    apostila(mss kubernetes): até 4.3.69.1 - arq. config. 1 pod         - retomar em arq. config. deployment pag. 117

--- hey_docker/commands.bash
+++ hey_docker/commands.bash
@@ -11,3 +11,19 @@ kubectl get deployments
 kubectl expose deployment/meu-primeiro-deployment --type="NodePort" --port 8080
 
 kubectl get services
+
+kubectl get pods -l app=meu-primeiro-deployment
+
+kubectl get services -l app=meu-primeiro-deployment
+
+kubectl get pods -o go-template --template "{{range .items}}{{.metadata.name}}{{end}}"
+
+kubectl label pod $POD_NAME versao=v1
+
+kubectl delete service -l app=meu-primeiro-deployment
+
+kubectl get service meu-primeiro-deployment -o go-template --template "{{(index .spec.ports 0)}}"
+
+kubectl set image deployments/meu-primeiro-deployment kubernetes-bootcamp=jocatalin/kubernetes-bootcamp:v2
+
+kubectl set image deployments/meu-primeiro-deployment kubernetes-bootcamp=gcr.io/google-samples/kubernetes-bootcamp:v10
